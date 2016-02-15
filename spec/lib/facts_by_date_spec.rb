7602e78020a483b4668f9faeb95cfad1d896ca64
Vasilis Kalligas <billkall@gmail.com>
2016-02-15 22:36:12 +0200

    Changed spec syntax to expect.

--- spec/lib/facts_by_date_spec.rb
+++ spec/lib/facts_by_date_spec.rb
@@ -5,29 +5,29 @@ describe FactsByDate::Generator do
     it 'should respond with full list when no option is given' do
       Timecop.freeze('02-01-2116')
       output = FactsByDate::Generator.facts_for_today
-      output.should be_kind_of Array
-      output.size.should == 14
+      expect(output).to be_kind_of Array
+      expect(output.size).to eq(14)
       Timecop.return
     end
 
     it 'should respond with proper list when size option is given' do
       output = FactsByDate::Generator.facts_for_today({:size => 2})
-      output.should be_kind_of Array
-      output.size.should == 2
+      expect(output).to be_kind_of Array
+      expect(output.size).to eq(2)
     end
   end
 
   describe '#facts_for_specific_date' do
     it 'should respond with proper list when no option is given' do
       output = FactsByDate::Generator.facts_for_specific_date('22-12-2015')
-      output.should be_kind_of Array
-      output.size.should == 20
+      expect(output).to be_kind_of Array
+      expect(output.size).to eq(20)
     end
 
     it 'should respond with proper list when size option is given' do
       output = FactsByDate::Generator.facts_for_specific_date('22-12-2015', { :size => 2 })
-      output.should be_kind_of Array
-      output.size.should == 2
+      expect(output).to be_kind_of Array
+      expect(output.size).to eq(2)
     end
   end
 end
